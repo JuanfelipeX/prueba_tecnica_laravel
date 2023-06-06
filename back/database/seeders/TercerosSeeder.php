f450602ceae179b65f5993be1caa6c750e9220cd
Juan Felipe Jimenez P <pipesjimenez6@gmail.com>
2023-06-06 16:48:40 -0500

    haciendo los seeder y modelos de terceros

---
+++ back/database/seeders/TercerosSeeder.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class TercerosSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        //
+    }
+}
