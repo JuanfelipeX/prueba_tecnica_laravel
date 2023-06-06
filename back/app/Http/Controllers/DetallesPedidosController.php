bd0d9637daf41f7d26822c10aacdd76e68b76a37
Juan Felipe Jimenez P <pipesjimenez6@gmail.com>
2023-06-06 16:51:27 -0500

    haciendo el modelo y seeder de usuarios de detalles_pedidos

---
+++ back/app/Http/Controllers/DetallesPedidosController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\detalles_pedidos;
+use Illuminate\Http\Request;
+
+class DetallesPedidosController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(detalles_pedidos $detalles_pedidos)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(detalles_pedidos $detalles_pedidos)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, detalles_pedidos $detalles_pedidos)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(detalles_pedidos $detalles_pedidos)
+    {
+        //
+    }
+}
